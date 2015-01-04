7e45b2fc5af9341f45962549f3d4dd8696892277
Gabriel Manricks <gmanricks@gmail.com>
2015-01-04 12:48:16 +0200

    Initial Commit

---
+++ mail_protocol/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  mail_protocol
+//
+//  Created by Gabriel Manricks on 1/4/15.
+//  Copyright (c) 2015 Gabriel Manricks. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
